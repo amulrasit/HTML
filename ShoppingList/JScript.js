2aa142690e4eadc73d9f16329cc0609808067e7b
amulrasit <amulrasit@gmail.com>
2014-08-22 13:29:34 -0500

    Shopping LIst

---
+++ ShoppingList/JScript.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    
+
+    $('#add').on('mouseover', function () {
+        $(this).css({ "background-color": "#FF5050" });
+    });
+
+    $('#add').on('mouseleave', function () {
+        $(this).css({ "background-color": "#FF9696" });
+    });
+    
+     $('input').on('focus', function () {
+        $('input').val('');
+    });   
+    
+    $('button').on('click', function ()
+     {
+       var itemName=$('input').val();
+       $('#shop ul').append("<li class='item'>"+itemName+"</li>");       
+       });
+       $('li').click(function(){
+       alert(this);
+        $(this).css({"text-decoration":"overline"});
+       });
+   });
