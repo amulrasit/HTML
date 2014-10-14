ec491b3b153c4330b6b4d05e9d300c57c59e21cc
amulrasit <amulrasit@gmail.com>
2014-10-14 12:52:37 -0500

    Portfolio

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    $('.spin').on('mouseover', function () {
+        $(this).css({ "background-color": "#FF5050" });
+    });
+
+    $('#PFSection').on('mouseenter', 'li', function () {
+
+        $(this).find(".text-content").addClass('descDisplay').slideDown(1000);
+    });
+
+    $('#PFSection').on('mouseleave', 'li', function () {
+        $(this).find(".text-content").removeClass('descDisplay').slideUp(1000);
+
+    });
+
+    $("#about").click(function (event) {
+        event.preventDefault();
+        $("#panelAbout").show();
+        $("#panelContact").hide();
+    });
+
+    $("#contact").click(function (event) {
+        event.preventDefault();
+        $("#panelAbout").hide();
+        $("#panelContact").show();
+    });
+
+    $("#port").click(function () {        
+        $("#panelAbout").hide();
+        $("#panelContact").hide();
+    });
+
+  });
+
+function test()
+{
+    alert("hi");
+}
